ad352e2741e43413331ffb76b5ceec2bb181fc80
Van Goghkhkh <vg219040@gmail.com>
2022-01-12 19:45:52 +0600

    done

--- tours_kg/models.py
+++ tours_kg/models.py
@@ -20,7 +20,7 @@ class Sight(models.Model):
     duration = models.PositiveIntegerField(default=0, verbose_name='Длительность')
 
     def __str__(self):
-        return self.province
+        return self.province, self.sight_name
 
     class Meta:
         verbose_name = 'Достопримечательность'
@@ -34,6 +34,9 @@ class BookNow(models.Model):
     email = models.EmailField(verbose_name='Ваша почта')
     sightseeing = models.ForeignKey(Sight, on_delete=models.CASCADE, verbose_name='Тур')
     quantity = models.PositiveIntegerField(default=1, verbose_name='Количество забронированных туров')
+    for_anonymous_user = models.BooleanField(default=False)
+
+
 
     class Meta:
         verbose_name = 'Забронировать'
@@ -44,6 +47,7 @@ class Users(models.Model):
     username = models.CharField(max_length=70, verbose_name='Как к вам обращаться?')
     user_phone = models.CharField(max_length=20, verbose_name='Ваш номер телефона')
     orders = models.ManyToManyField(BookNow, verbose_name='Ваши брони', related_name='related orders')
+
 
 
 
