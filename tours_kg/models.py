c9df56226a615f1420a068b2d221ab654a50c41b
Aybek18 <kasymaliev09@gmail.com>
2022-01-12 18:45:44 +0600

    dfd

--- tours_kg/models.py
+++ tours_kg/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db import models
+
+class Region(models.Model):
+    reg_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.reg_name
+
+
+class Sight(models.Model):
+    province = models.ForeignKey(Region, max_length=40, on_delete=models.CASCADE)
+    description = models.TextField(verbose_name='Описание')
+    price = models.IntegerField(default=0)
+    duration = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.province
+
+
+class BookNow(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(verbose_name='ИМАИЛ')
+    sightseeing = models.ForeignKey(Sight, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
 
 # Create your models here.
